7943fd7a5179db6d004d3cc252b5bba7869553c6
sabir kral <sabirmemmedli21152014@gmail.com>
2026-04-25 17:06:16 +0400

    asd

--- demo/src/main/java/az/computer/demo/Request/AddVerifyRequest.java
+++ demo/src/main/java/az/computer/demo/Request/AddVerifyRequest.java
@@ -1,10 +1,14 @@
 package az.computer.demo.Request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AddVerifyRequest {
 
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("code")
     private String code;
 }
