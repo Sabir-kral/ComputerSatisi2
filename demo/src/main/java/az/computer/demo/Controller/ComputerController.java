39e94afbd7388c84c230086ffc642aa2e26e9dd7
sabir kral <sabirmemmedli21152014@gmail.com>
2026-09-01 23:01:28 +0400

    asdss

--- demo/src/main/java/az/computer/demo/Controller/ComputerController.java
+++ demo/src/main/java/az/computer/demo/Controller/ComputerController.java
@@ -5,7 +5,6 @@ import az.computer.demo.Response.ComputerResponse;
 import az.computer.demo.Response.MessageResponse;
 import az.computer.demo.Service.ComputerService;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.annotation.DeclareWarning;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,9 +20,10 @@ public class ComputerController {
     }
 
     @PutMapping("/{id}")
-    public MessageResponse update(@PathVariable Long id,@RequestBody ComputerRequest request){
-        return computerService.update(id,request);
+    public MessageResponse update(@PathVariable Long id, @RequestBody ComputerRequest request){
+        return computerService.update(id, request);
     }
+
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         computerService.delete(id);
